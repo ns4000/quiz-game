4a4ebbdc5fcc33340a7c2378b822735043fa25d9
ns4000 <ns40002@gmail.com>
2017-09-06 23:45:52 +0200

    working on landing page working prog

--- src/component/landingPage/LandingPage.jsx
+++ src/component/landingPage/LandingPage.jsx
@@ -20,7 +20,7 @@ constructor(props){
       toggleconfig=( <form className="form-control" onSubmit={(e)=>{e.preventDefault();this.props.onStartgame(this.state.user)}}>
         <input className="form-control text-center" type="text"      placeholder="Enter user name or enter as guest" onChange={(e)=>this.setState({user:e.target.value})} required/>
         <input className="form-control btn-success" type="submit"    defaultValue="Start Quiz"/>
-        <input className="form-control btn-warning" type="button"    defaultValue="midget" onClick={(e)=>{e.preventDefault();this.props.onStartgame("Guest")}}/>
+        <input className="form-control btn-warning" type="button"    defaultValue="Guest" onClick={(e)=>{e.preventDefault();this.props.onStartgame("Guest")}}/>
       </form>)
     }
     return(
